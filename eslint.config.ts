436973a62e4bea3f3fda9cebcec82b29a06110e0
Andrii <wizardionious.education@gmail.com>
2026-02-07 20:46:17 +0200

    formatted

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,13 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import pluginPlaywright from 'eslint-plugin-playwright'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginOxlint from 'eslint-plugin-oxlint'
-import skipFormatting from 'eslint-config-prettier/flat'
+import pluginVitest from "@vitest/eslint-plugin";
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from "@vue/eslint-config-typescript";
+import skipFormatting from "eslint-config-prettier/flat";
+import pluginOxlint from "eslint-plugin-oxlint";
+import pluginPlaywright from "eslint-plugin-playwright";
+import pluginVue from "eslint-plugin-vue";
+import { globalIgnores } from "eslint/config";
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -13,26 +16,30 @@ import skipFormatting from 'eslint-config-prettier/flat'
 
 export default defineConfigWithVueTs(
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{vue,ts,mts,tsx}'],
+    name: "app/files-to-lint",
+    files: ["**/*.{vue,ts,mts,tsx}"],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores([
+    "**/dist/**",
+    "**/dist-ssr/**",
+    "**/coverage/**",
+  ]),
 
-  ...pluginVue.configs['flat/essential'],
+  ...pluginVue.configs["flat/essential"],
   vueTsConfigs.recommended,
 
   {
-    ...pluginPlaywright.configs['flat/recommended'],
-    files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    ...pluginPlaywright.configs["flat/recommended"],
+    files: ["e2e/**/*.{test,spec}.{js,ts,jsx,tsx}"],
   },
 
   {
     ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
+    files: ["src/**/__tests__/*"],
   },
 
-  ...pluginOxlint.configs['flat/recommended'],
+  ...pluginOxlint.configs["flat/recommended"],
 
   skipFormatting,
-)
+);
