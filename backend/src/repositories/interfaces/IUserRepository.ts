7077e7a3e40fbd3a764bf897ca120705fa145f8c
Artem Levchenko <levchencode@gmail.com>
2026-02-20 22:29:46 +0200

    feature: repository pattern

---
+++ backend/src/repositories/interfaces/IUserRepository.ts
@@ -0,0 +1,13 @@
+import type { IUser } from "@/models/User";
+
+export interface CreateUserData {
+    email: string;
+    password: string;
+    name: string;
+}
+
+export interface IUserRepository {
+    findByEmail(email: string): Promise<IUser | null>;
+    findById(id: string): Promise<IUser | null>;
+    create(data: CreateUserData): Promise<IUser>;
+}
